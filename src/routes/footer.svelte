1a8e7da0fa98562a807d8595c4b675f60b83258c
dj0le <gstar12@hotmail.com>
2023-09-11 10:41:21 +0200

    added hooks server page

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,6 +1,4 @@
-<div
-	class="p-4 tracking-wider font-title text-center bg-gradient-to-br from-grad-left1 from-40% to-grad-right1 to-100% shadow-card"
->
+<div class="p-4 tracking-wider font-title text-center">
 	<div class="text-brand1 flex flex-col sm:flex-row justify-center">
 		<a href="/" class="pr-0 sm:pr-4" data-sveltekit-preload-data>
 			Tournament<span class="text-brand2">Elo</span></a
