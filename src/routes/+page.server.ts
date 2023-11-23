ea89f9f093f1a2dacd156751e2b28b09e78f9147
simonsan <14062932+simonsan@users.noreply.github.com>
2023-11-23 14:46:42 +0100

    add example for tournament and team caches
    Signed-off-by: simonsan <14062932+simonsan@users.noreply.github.com>

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,5 +4,13 @@ import { models } from "./hooks.server";
 // console.log(users);
 
 const daut = await models.player.findOne({ where: { id: 1 }, include: ["country"] });
-
 console.log(JSON.stringify(daut, null, 2));
+
+const all_players_cache = await models.player.findAll({ attributes: ["id", "name"] });
+console.log(JSON.stringify(all_players_cache, null, 2));
+
+const all_teams_cache = await models.team.findAll({ attributes: ["id", "name"] });
+console.log(JSON.stringify(all_teams_cache, null, 2));
+
+const all_tounaments_cache = await models.tournament.findAll({ attributes: ["id", "name"] });
+console.log(JSON.stringify(all_tounaments_cache, null, 2));
