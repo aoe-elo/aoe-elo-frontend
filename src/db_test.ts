eede82067a24fa30a2337472e7718a447b8bc733
simonsan <14062932+simonsan@users.noreply.github.com>
2023-11-23 12:14:56 +0100

    start with sequelize orm
    Signed-off-by: simonsan <14062932+simonsan@users.noreply.github.com>

---
+++ src/db_test.ts
@@ -0,0 +1,8 @@
+import { sequelize } from "$lib/sequelize";
+
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
